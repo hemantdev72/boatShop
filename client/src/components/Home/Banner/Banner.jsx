a8d907780a9d509efda4783cc8bc9eb986d96fc3
hemantdev72 <hemantdev72@gmail.com>
2024-01-07 19:22:47 +0530

    initial commit

---
+++ client/src/components/Home/Banner/Banner.jsx
@@ -0,0 +1,24 @@
+import "./Banner.scss";
+import BannerImg from "../../../assets/banner-img.png";
+const Banner = () => {
+  return (
+    <div className="hero-banner">
+      <div className="content">
+        <div className="text-content">
+          <h1>SALES</h1>
+          <p>
+            Convallis interdum purus adipiscing dis parturient posure ac a quwam
+            a eleifend montes parturient posure curae tempor
+          </p>
+          <div className="ctas">
+            <div className="banner-cta">Read More</div>
+            <div className="banner-cta v2">Shop Now</div>
+          </div>
+        </div>
+        <img src={BannerImg} alt="main image" className="banner-img" />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ client/src/components/Products/Product/Product.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./Product.scss";
+
+const Product = ({ data, id }) => {
+  // console.log(data);
+  const navigate = useNavigate();
+  const imageUrl =
+    data && data.img && data.img.data && data.img.data[0]
+      ? data.img.data[0].attributes.url
+      : "fallback-url";
+  return (
+    <div className="product-card" onClick={() => navigate("/product/" + id)}>
+      <div className="thumbnail">
+        <img src={"http://localhost:1337" + imageUrl} />
+      </div>
+      <div className="prod-details">
+        <span className="name">{data.title}</span>
+        <span className="price">&#8377;{data.price}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
